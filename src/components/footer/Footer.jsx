b81c09ec888d5ed23ee4bda6448cf0b5aa7894d8
olafnub <googlhoogle@gmail.com>
2022-12-29 14:01:36 -0600

    Finish footer section
    Why - Show users credits and also another nav link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 import './footer.css'
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+   <footer>
+    <a href="#" className="footer__logo">React Portfolio Helped Made BY <a href="https://youtu.be/G-Cr00UYokU">EGATOR</a></a>
+
+    <ul className="permalinks">
+      <li><a href="#">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="portfolio">Portfolio</a></li>
+      <li><a href="#miniblog">Miniblog</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+
+    <div className="footer__copyright">
+      <small>Followed youtube tutorial by <a href="https://youtu.be/G-Cr00UYokU">EGATOR</a></small>
+    </div>
+
+
+    </footer>
   )
 }
 
